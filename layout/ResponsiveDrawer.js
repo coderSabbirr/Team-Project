0bca54e8ea27f77f5992a027193968fad9a59f76
nub69 <shabik.ashab@gmail.com>
2022-03-12 17:31:07 +0600

    dashboard appbar added

---
+++ layout/ResponsiveDrawer.js
@@ -0,0 +1,194 @@
+import * as React from 'react';
+import Link from 'next/link'
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import drawerStyle from '../styles/drawer.module.scss'
+
+const drawerWidth = 240;
+
+export function ResponsiveDrawer({window,children}) {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+       
+     <Box sx={{p:2,fontSize:'1.3em'}} textAlign="center">
+         admin
+     </Box>
+     
+      <Divider />
+      <Link href="/dash">
+      <Box
+          sx={{
+            backgroundColor: "",
+            "&:hover": {
+              backgroundColor: "#e0e0e0",
+              opacity: [0.9, 0.8, 0.7],
+            },
+            p: 1,
+            m: 1,
+            cursor:'pointer'
+          }}
+          textAlign="center"
+        >
+          <span> dash</span>
+        </Box>
+        </Link>
+        <Link href="/dash/myOrder">
+        <Box
+          sx={{
+            backgroundColor: "",
+            "&:hover": {
+              backgroundColor: "#e0e0e0",
+              opacity: [0.9, 0.8, 0.7],
+            },
+            p: 1,
+            m: 1,
+            cursor:'pointer'
+          }}
+          textAlign="center"
+        >
+          <span> My order</span>
+        </Box>
+        </Link>
+      <Divider />
+     
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+          backgroundColor:'transparent',
+          boxShadow:'none',
+          color:"black"  
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+ 
+          <div className={drawerStyle.navbar}>
+    <div className={drawerStyle.wrapper}>
+      <div className={drawerStyle.search}>
+        <input type="text" placeholder='Search...' />
+        <SearchOutlinedIcon />
+        </div>
+        <div className={drawerStyle.items}>
+          <div className={drawerStyle.item} >
+            <LanguageOutlinedIcon className={drawerStyle.icon} />
+            English
+            </div>
+            <div className={drawerStyle.item}>
+              <DarkModeOutlinedIcon className={drawerStyle.icon}  />
+              </div>
+              <div className={drawerStyle.item}>
+              <FullscreenExitOutlinedIcon className={drawerStyle.icon}  />
+              </div>
+              <div className={drawerStyle.item}>
+              <NotificationsNoneOutlinedIcon className={drawerStyle.icon}  />
+              <div className={drawerStyle.counter} >1</div>
+              </div>
+              <div className={drawerStyle.item}>
+              <ChatBubbleOutlineOutlinedIcon className={drawerStyle.icon}  />
+              <div className={drawerStyle.counter} >2</div>
+              </div>
+              <div className={drawerStyle.item}>
+              <ListOutlinedIcon className={drawerStyle.icon}  />
+              </div>
+              <div className={drawerStyle.item}>
+            <img
+              src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+              className={drawerStyle.avatar}
+            />
+          </div>
+          </div>
+      </div>  
+    </div>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+        {children}
+      </Box>
+    </Box>
+  );
+}
+
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
---
+++ pages/dash/myOrder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const myOrder = () => {
+  return (
+    <div>myOrder</div>
+  )
+}
+
+export default myOrder
