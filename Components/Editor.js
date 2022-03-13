ccd2dfdedfb094b92e424ef8afe3f17a7820c608
nub69 <shabik.ashab@gmail.com>
2022-03-13 09:56:15 +0600

    ckeditor fix

--- Components/Editor.js
+++ Components/Editor.js
@@ -2,15 +2,28 @@ import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 
 const Editor = () => {
     const [value,setValue] = useState('');
+    const [parseValue,setParseValue] = useState([]);
+
+    // const [backendData, setBackendData] = useState({ type:parseValue[0]?.type,
+    //     data: parseValue[0]?.props?.children[0]});
 
     const handleChange = (e, editor) => {
-        console.log(editor.getData());
+        // console.log(e);
+        const data = editor.getData();
+        setValue(data)
+        setParseValue(ReactHtmlParser(data));
     }
 
+    const backendData={
+        jsonData:value
+    }
+   
+    
   return (
     <>
         <Container>
@@ -18,6 +31,9 @@ const Editor = () => {
             editor={ClassicEditor}
             onChange={handleChange}
             />
+            <div>
+                {ReactHtmlParser(value)}
+            </div>
         </Container>
     </>
   )
