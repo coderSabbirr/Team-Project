3c043dca9b084acef390149f560af31cd3d416db
nub69 <shabik.ashab@gmail.com>
2022-03-14 18:22:22 +0600

    freelancer details gallery added

--- pages/freelancer/[id]/index.js
+++ pages/freelancer/[id]/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Grid from '@mui/material/Grid';
-import style from '../../../styles/frellancerDetails.module.scss'
-
-
+import style from '../../../styles/frellancerDetails.module.scss';
+import galleryStyle from '../../../Components/PhotoCollection/PhotoCollection.module.css'
+import Singlephoto from '../../../Components/PhotoCollection/Singlephoto'
+import Button from '@mui/material/Button';
 
 const freelancers=[
     {
@@ -44,18 +45,98 @@ const freelancers=[
     },
 ]
 
+let photosGallery = [
+    {
+        img: 'https://i.ibb.co/M7wVVxX/pexels-bess-hamiti-35537.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/86bb03s/pexels-andre-moura-3334450.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/VmGvfDv/pexels-luizclas-556669.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/4TDvS8f/pexels-ruvim-3622517.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/CMP65kY/pexels-frans-van-heerden-2253391.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/VmGvfDv/pexels-luizclas-556669.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/4TDvS8f/pexels-ruvim-3622517.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/CMP65kY/pexels-frans-van-heerden-2253391.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/M7wVVxX/pexels-bess-hamiti-35537.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/86bb03s/pexels-andre-moura-3334450.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/VmGvfDv/pexels-luizclas-556669.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/4TDvS8f/pexels-ruvim-3622517.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/CMP65kY/pexels-frans-van-heerden-2253391.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/VmGvfDv/pexels-luizclas-556669.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/4TDvS8f/pexels-ruvim-3622517.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/CMP65kY/pexels-frans-van-heerden-2253391.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/M7wVVxX/pexels-bess-hamiti-35537.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/86bb03s/pexels-andre-moura-3334450.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/VmGvfDv/pexels-luizclas-556669.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/4TDvS8f/pexels-ruvim-3622517.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/CMP65kY/pexels-frans-van-heerden-2253391.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/VmGvfDv/pexels-luizclas-556669.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/4TDvS8f/pexels-ruvim-3622517.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/CMP65kY/pexels-frans-van-heerden-2253391.jpg'
+    },
+    {
+        img: 'https://i.ibb.co/M7wVVxX/pexels-bess-hamiti-35537.jpg'
+    },
+]
+
 
 const frellancerDetails = () => {
     const router = useRouter();
     const {id} = router.query;
+    const copyData = photosGallery;
+    const [galleryImg,setGalleryImg] = React.useState(photosGallery);
     
     const currentFreelancer = freelancers.find((freelancer) => freelancer.id === id);
-
-    console.log(currentFreelancer)
+  galleryImg.splice(0,4);
+    console.log(copyData)
   return (
     <div className={style.freelancer} > frellancerDetails 
          <Grid sx={{mt:12,ml:6}} container spacing={2}>
-        <Grid className={style.profile} item md={4} xs={6}>
+        <Grid className={style.profile} item md={4} xs={12}>
           <div className={style.profileImg}>
             <img src={currentFreelancer?.img} />      
         </div> 
@@ -66,10 +147,25 @@ const frellancerDetails = () => {
             <div>{currentFreelancer?.title}</div>
         </div>
         </Grid>
-        <Grid item md={6} xs={12}>
+        <Grid item md={0} xs={0}>
           
         </Grid>
       </Grid>
+        <div style={{marginTop:'40px'}}>
+        <div className={galleryStyle.photos}>
+                {
+                    galleryImg?.map(photo=> 
+                        <Singlephoto key={photo.img} photo={photo}/>
+                        )
+                }
+            </div>
+            {
+            copyData.length >5 &&
+            <Button variant="outlined" >
+             See all image
+            </Button>
+            }
+        </div>
     </div>
   )
 }
